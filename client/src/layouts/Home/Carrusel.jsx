d359f98ad0967efdc1437669f66f80f6ac54b0af
JoseChkon <jose.a.chacon.g.2005@gmail.com>
2024-02-22 13:21:28 -0400

    Final-Auth-Google

--- client/src/layouts/Home/Carrusel.jsx
+++ client/src/layouts/Home/Carrusel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "../../components/Card";
 
 export default function Gallery() {
-  const carouselBlogs = [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5},{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}];
+  const carouselBlogs = [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5},{id: 6}, {id: 7}, {id: 8}, {id: 9}, {id: 10}];
   return (
     <div className="my-5">
       <div className="overflow-hidden w-full">
