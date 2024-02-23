24347719a71417bcf065156913ec5c28aecd0229
JoseChkon <152924463+JoseChkon@users.noreply.github.com>
2024-02-23 14:18:59 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ app.use(passport.session())
 app.use(morgan("dev"))
 
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Origin', "https://frontend-h6od.onrender.com/");
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
     next();
