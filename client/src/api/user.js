ef79abb3c3e32ca1a74cf338e0d997e690f2c384
JoseChkon <152924463+JoseChkon@users.noreply.github.com>
2024-02-23 14:15:00 -0400

    Update user.js

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -1,5 +1,5 @@
 import axios from './axios.js'
 
-export const userRequest = () => axios.get("/auth/user");
+export const userRequest = () => axios.get("/auth/user",{headers: {'Access-Control-Allow-Origin': '*'}});
 
 export const logoutRequest = () => axios.delete("/auth/logout")
