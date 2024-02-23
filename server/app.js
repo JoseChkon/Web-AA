4e5cbd324c30c429b40fc9c557eff2ad2d134021
JoseChkon <152924463+JoseChkon@users.noreply.github.com>
2024-02-23 14:11:25 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -31,6 +31,13 @@ app.use(passport.session())
 
 app.use(morgan("dev"))
 
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use("/", routes)
 
 module.exports = app
